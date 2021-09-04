d8b84e6a66df9dd5cd9dc7327fa58b24a34a612c
Thiago Correia <dasilvacorreiathiago@gmail.com>
2021-09-04 12:06:24 -0300

    script php

--- cadastra.php
+++ cadastra.php
@@ -1,2 +1,15 @@
+<?php
+  //Definição de variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $senha = $_POST['senha'];
 
+  if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if(! isset($_COOKIE["cookieName"])){
+      setcookie("nome", $nome,  0, "/", "", False, False);
+    }
+  }else{
+    echo "Método errado"
+  }
+?>
 
